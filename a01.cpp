aa8dcfaa404c3f37c8545006b8cfa387c0e5b558
NearLawiet <sooraj1999vs@gmail.com>
2019-12-20 22:07:09 +0530

    Completed Traversals

--- a01.cpp
+++ a01.cpp
@@ -79,15 +79,116 @@ void Tree::traverseInorderRecursive(Node *p){
 	}
 	return;
 }
+void Tree::inorderIterative(){
+	if(root==NULL){
+		cout<<"\nTree is Empty!";
+		return;		
+	}
+	cout<<"\n";
+	Node *p=root;
+	stack<Node *> stack;
+	while(p!=NULL||!stack.empty()){
+		while(p!=NULL){
+			stack.push(p);
+			p=p->left;
+		}
+		p=stack.top();
+		stack.pop();
+		cout<<p->data<<" ";
+		p=p->right;
+	}
+
+}
+void Tree::passPreorderRecursive(){
+	if(root==NULL){
+		cout<<"\nTree is Empty!";
+		return;
+	}
+	cout<<"\n";
+	traversePreorderRecursive(root);
+}
+void Tree::traversePreorderRecursive(Node *p){
+	if(p!=NULL){
+		cout<<p->data<<" ";
+		traversePreorderRecursive(p->left);
+		traversePreorderRecursive(p->right);
+	}
+	return;
+}
+void Tree::preorderIterative(){
+	if(root==NULL){
+		cout<<"\nTree is Empty!";
+		return;		
+	}
+	cout<<"\n";
+	Node *p=root;
+	stack<Node *> stack;
+	while(p!=NULL||!stack.empty()){
+		while(p!=NULL){
+			cout<<p->data<<" ";
+			stack.push(p);
+			p=p->left;
+		}
+		p=stack.top();
+		stack.pop();
+		p=p->right;
+	}
+}
+void Tree::passPostorderRecursive(){
+	if(root==NULL){
+		cout<<"\nTree is Empty!";
+		return;
+	}
+	cout<<"\n";
+	traversePostorderRecursive(root);
+}
+void Tree::traversePostorderRecursive(Node *p){
+	if(p!=NULL){
+		traversePostorderRecursive(p->left);
+		traversePostorderRecursive(p->right);
+		cout<<p->data<<" ";
+	}
+}
+void Tree::postorderIterative(){
+	if(root==NULL){
+		cout<<"\nTree is Empty!";
+		return;		
+	}
+	cout<<"\n";
+	stack<Node *> stack1,stack2;
+	Node *p=root;
+	stack1.push(p);
+	while(!stack1.empty()){
+		p=stack1.top();
+		stack1.pop();
+		stack2.push(p);
+		if(p->left!=NULL)
+			stack1.push(p->left);
+		if(p->right!=NULL)
+			stack1.push(p->right);
+	}
+	while(!stack2.empty()){
+		p=stack2.top();
+		stack2.pop();
+		cout<<p->data<<" ";
+	}
+}
 int main(){
 	Tree container;
-	container.insert(12);
-	container.insert(13);
-	container.insert(14);
-	container.insert(15);
-	container.insert(16);
-	container.insert(17);
+	container.insert(1);
+	container.insert(2);
+	container.insert(3);
+	container.insert(4);
+	container.insert(5);
+	container.insert(6);
+	container.insert(7);
+	//container.insert(26);
 	container.passInorderRecursive();
+	container.inorderIterative();
+	container.passPreorderRecursive();
+	container.preorderIterative();
+	container.passPostorderRecursive();
+	container.postorderIterative();
 	return 0;
 }
 
