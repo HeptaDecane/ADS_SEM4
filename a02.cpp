03d5d5d226c328a893c78e15f3eeb8848259235f
NearLawiet <sooraj1999vs@gmail.com>
2020-01-06 12:51:47 +0530

    Dictionary Using BST

--- a02.cpp
+++ a02.cpp
@@ -40,6 +40,8 @@ public:
 	void callReverseInorder();
 	void updateMeaning(char[],string);
 	void operator =(Tree&);
+	void callDeleteNode(char[]);
+	Node *deleteNode(Node*,char[]);
 	void menu();
 };
 Tree::Tree(){
@@ -144,6 +146,52 @@ Node* Tree::setEqual(Node *original,Node *copy){
 	}
 	return copy;
 }
+void Tree::callDeleteNode(char keyword[MAX]){
+	root=deleteNode(root,keyword);
+}
+Node* Tree::deleteNode(Node *p,char keyword[MAX]){
+	if(p==NULL){
+		cout<<"\n"<<keyword<<" Not Found";
+		return NULL;
+	}
+	if(strcmp(keyword,p->keyword)<0){
+		p->left=deleteNode(p->left,keyword);
+		return p;
+	}
+	else if(strcmp(keyword,p->keyword)>0){
+		p->right=deleteNode(p->right,keyword);
+		return p;
+	}
+	//Node to be Deleted is Reached (data==p->data)
+	cout<<"\n"<<keyword<<" Deleted";
+	//If p has NO or only ONE child
+	if(p->left==NULL){
+		Node *temp=p->right;
+		delete p;
+		return temp;
+	}
+	else if(p->right==NULL){
+		Node *temp=p->left;
+		delete p;
+		return temp;
+	}
+	//If p has two children
+	else{
+		Node *ancestor=p->right;
+		Node *successor=p->right;
+		while(successor->left!=NULL){
+			ancestor=successor;
+			successor=successor->left;
+		}
+		if(ancestor==successor)
+			p->right=NULL;
+		strcpy(p->keyword,successor->keyword);
+		p->meaning=successor->meaning;
+		ancestor->left=successor->right;
+		delete successor;
+		return p;
+	}
+}
 void Tree::menu(){
 	cout<<"\n------------------------------------------------------";
 	cout<<"\n           OPTION INDEX";
@@ -192,7 +240,9 @@ int main(){
 					getline(cin,meaning);
 					dictionary.updateMeaning(keyword,meaning);
 					break;
-			case 7: cout<<"\nWork In Progress! ";
+			case 7: cout<<"\nEnter The Keyword to be Deleted: ";
+					cin>>keyword;
+					dictionary.callDeleteNode(keyword);
 					break;
 			case 99:cc.menu();
 					break;
@@ -201,255 +251,3 @@ int main(){
 	}
 	return 0;
 }
-
-/*
-#include <iostream.h>
-#include<conio.h>
-#include <string.h>
-#define MAX 10
-class node
-{
-private:
- char keyword[MAX];
- char meaning[MAX];
- node *left;
- node *right;
-public:
- node();
- node(char [],char []);
- friend class BST;
-};
-node::node()
-{
- keyword[0] = '\0';
- meaning[0] = '\0';
- left = NULL;
- right = NULL;
-}
-node::node(char key[],char mean[])
-{
- strcpy(keyword,key);
- strcpy(meaning,mean);
- left = NULL;
- right = NULL;
-}
-class BST
-{
-private:
- node *root;
- void inorder(node *root);
- void inorderrev(node *root);
- node * insert(node * root,node *);
- node* search(char key[]);
- node * remove(node *root,char key[]);
-public:
- BST();
- void update(char []);
- void recInsert(char [] , char[]);
- node *search(node *, char []);
- void find(char key[]);
- void printAscending();
- void printDescending();
- void removeword(char val[]);
-};
-BST::BST()
-{
- root = NULL;
-}
-void BST::recInsert(char k[], char m[])
-{
- node *newnd=new node(k,m);
- root = insert(root,newnd);
-}
-node * BST::insert(node * root,node *newnd)
-{
- if(root==NULL)
-  root = newnd;
- else if (strcmp(newnd->keyword, root->keyword)<0)
-  root->left=insert(root->left,newnd);
- else if (strcmp(newnd->keyword, root->keyword)>0)
-  root->right=insert(root->right,newnd);
- else
-  cout<<"\nDuplicate value";
- return root;
-}
-
-node * BST::remove(node *root,char key[])
-{
- if(root==NULL)
-  return NULL;
- else if (strcmp(key,root->keyword)<0)
-  root->left=remove(root->left,key);
- else if (strcmp(key,root->keyword)>0)
-  root->right=remove(root->right,key);
- else
- {
-  if(root->right!=NULL)
-  {
-   node *in_succ = root->right;
-   while(in_succ->left!=NULL)
-   {
-    in_succ = in_succ->left;
-   }
-
-   strcpy(root->keyword,in_succ->keyword);
-   strcpy(root->meaning,in_succ->meaning);
-   root->right=remove(root->right,in_succ->keyword);
-  }
-  else
-   return root->left;
- }
- return root;
-}
-void BST::removeword(char val[])
-{
- remove(root,val);
-}
-void BST::update(char k[])
-{
- node *tempnd;
- tempnd = search(k);
- if(tempnd == NULL)
-  cout<<"\nWord not present for Update:";
- else
- {
-  cout<<"\nEnter new meaning for this word";
-  cin>>tempnd->meaning;
- }
-}
- node *BST::search(char val[])
- {
- node *tempnd=root;
- tempnd=search(tempnd,val);
- return tempnd;
- }
- node *BST::search(node *tempnd, char k[])
- {
- if(tempnd!= NULL)
- {
-  if(strcmp(k,tempnd->keyword)==0)
-   return tempnd;
-  else if(strcmp(k, tempnd->keyword)<0)
-   search(tempnd->left, k);
-  else
-   search(tempnd->right, k);
- }//if
- else
-  return NULL; //not found
-}
-void BST::find(char val[])
-{
- node *tempnd;
- tempnd=search(val);
- if (tempnd == NULL)
- {
-  cout<<endl<<"Not found\n";
- }
- else
- {
-  cout<<endl<<"Found...";
-  cout<<"\nMeaning is "<<tempnd->meaning;
- }
-}//find
-void BST::inorder(node *root)
-{
- if(root!= NULL)
- {
-  inorder(root->left);
-  cout<<"\n"<<root->keyword<<":"<<root->meaning;
-  inorder(root->right);
- }
-}
-void BST::inorderrev(node *root)
-{
- if(root!= NULL)
- {
-  inorderrev(root->right);
-  cout<<"\n"<<root->keyword<<":"<<root->meaning;
-  inorderrev(root->left);
- }
-}
-void BST::printAscending()
-{
- inorder(root);
-}
-void BST::printDescending()
-{
- inorderrev(root);
-}
-int menu()
-{
- int choice;
- cout<<"\nD I C T I O N A R Y A P P L I C A T I O N";
- cout<<"\n\t1. Insert ";
- cout<<"\n\t2. Update ";
- cout<<"\n\t3. Delete ";
- cout<<"\n\t4. Print Ascending ";
- cout<<"\n\t5. Print Descending";
- cout<<"\n\t6. Find ";
- cout<<"\n\t7. Exit ";
- cout<<"\nEnter your Choice \t";
- cin>>choice;
- return choice;
-}//menu
-
-void main()
-{
- BST t;
- char k[MAX],m[MAX];
- clrscr();
- char keyword[40];
- int n,choice;
- while(1)
- {
-  choice=menu();;
-  switch(choice)
-  {
-  case 1: cout<<"\nHow many values to insert";
-   cin>>n;
-   for(int i=0;i<n;i++)
-   {
-    cout<<"\nEnter new Keyword :";
-    cin>>k;
-    cout<<"\nEnter meaning of "<<keyword<<" : ";
-    cin>>m;
-    t.recInsert(k,m);
-   }
-   break;
-  case 2: cout<<"\nEnter keyword to be found:";
-   cin>>k;
-   t.update(k);
-   break;
-  case 3: cout<<"Enter the keyword :";
-   cin>>keyword;
-   t.removeword(keyword);
-   break;
-  case 4: cout<<"\nPrint Dictionary in Ascending Order: ";
-   t.printAscending();
-   break;
-  case 5: cout<<"\nPrint Dictionary in Descending Order: ";
-   t.printDescending();
-   break;
-  case 6: cout<<"Enter the keyword :";
-   cin>>keyword;
-   t.find(keyword);
-   break;
-  case 7: cout <<"\nProgram ending....\n";
-   getch();
-   return;
-  default: cout <<"\nEnter correct choice...\n";
-  }//switch
- }//while
-}//main
-*/
-
-
-
-
-
-
-
-
-
-
-
