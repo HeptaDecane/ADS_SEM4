5dfb2216247b05d398080ecde0e32daf10a147b6
NearLawiet <sooraj1999vs@gmail.com>
2020-01-17 15:43:55 +0530

    Traversal Remains

--- a04.cpp
+++ a04.cpp
@@ -31,6 +31,8 @@ public:
 	bool deleteNode(string);
 	void erase();
 	void display();
+	bool search(string);
+	int count();
 	friend class Graph;
 };
 List::List(){
@@ -43,7 +45,7 @@ void List::insert(string city,float cost){
 		return;
 	}
 	Node *p;
-	for(p=start;p->link!=NULL;p=p->link)
+	for(p=start;p->link!=NULL;p=p->link);
 	p->link=temp;
 }
 bool List::deleteNode(string city){
@@ -59,6 +61,12 @@ bool List::deleteNode(string city){
 	delete q;
 	return true;
 }
+int List::count(){
+	int count=0;
+	for(Node *p=start->link;p!=NULL;p=p->link)
+		count++;
+	return count;
+}
 void List::erase(){
 	Node *p=start;
 	Node *temp=NULL;
@@ -68,6 +76,14 @@ void List::erase(){
 		temp->link=NULL;
 		delete temp;
 	}
+}
+bool List::search(string city){
+	Node *p;
+	for(p=start->link;p!=NULL;p=p->link){
+		if(p->city==city)
+			return true;
+	}
+	return false;
 }
 void List::display(){
 	for(Node *p=start;p!=NULL;p=p->link)
@@ -83,6 +99,9 @@ public:
 	void addEdge(string,string,float);
 	bool deleteVertex(string);
 	bool deleteEdge(string,string);
+	int outDegree(string);
+	int inDegree(string);
+	void menu();
 	void display();
 };
 Graph::Graph(){
@@ -98,17 +117,137 @@ bool Graph::addVertex(string city){
 	count++;
 	return true;
 }
+void Graph::addEdge(string source,string destination,float cost){
+	int i,j;
+	for(i=0;i<count;i++){
+		if(array[i].start->city==source)
+			break;
+	}
+	for(j=0;j<count;j++){
+		if(array[j].start->city==destination)
+			break;
+	}
+	if(i==count||j==count||i==j){
+		cout<<"\nInvalid Source and/or Destination";
+		return;
+	}
+	array[i].insert(destination,cost);
+}
 void Graph::display(){
 	for(int i=0;i<count;i++){
 		cout<<"\n";
 		array[i].display();
 	}
 }
-int main() {
-	Graph flights;
-	flights.addVertex("Pune");
-	flights.addVertex("Mumbai");
-	flights.addVertex("Nashik");
-	flights.display();
-	return 0;
+bool Graph::deleteEdge(string source,string destination){
+	int i;
+	for(i=0;i<count;i++){
+		if(array[i].start->city==source)
+			break;
+	}
+	if(i==count){
+		cout<<"\nSource: "<<source<<" Doesn't Exist!";
+		return false;
+	}
+	return array[i].deleteNode(destination);
 }
+bool Graph::deleteVertex(string city){
+	int i;
+	for(i=0;i<count;i++){
+		if(array[i].start->city==city)
+			break;
+	}
+	if(i==count){
+		cout<<"\nCity: "<<city<<" Doesn't Exist!";
+		return false;
+	}
+	array[i].erase();
+	count--;
+	for(int j=i;j<count;j++){
+		array[j]=array[j+1];
+	};
+	for(int j=0;j<count;j++){
+		array[j].deleteNode(city);
+	}
+	return true;
+}
+int Graph::outDegree(string city){
+	int i;
+	for(i=0;i<count;i++){
+		if(array[i].start->city==city)
+			break;
+	}
+	if(i==count){
+		cout<<"\nCity: "<<city<<" Doesn't Exist!";
+		return -1;
+	}
+	return array[i].count();
+}
+int Graph::inDegree(string city){
+	int i,cnt=0;
+	for(i=0;i<count;i++){
+		if(array[i].start->city==city)
+			break;
+	}
+	if(i==count){
+		cout<<"\nCity: "<<city<<" Doesn't Exist!";
+		return -1;
+	}
+	for(int i=0;i<count;i++){
+		if(array[i].search(city)==true)
+			cnt++;
+	}
+	return cnt;
+}
+void Graph::menu(){
+	cout<<"\n--------------------------OPTION INDEX--------------------------";
+	cout<<"\nPress 1: Add City in DataBase (Add Vertex in Graph)";
+	cout<<"\nPress 2: Connect Two Cities (Add Edge in Graph)";
+	cout<<"\nPress 3: DisConnect Two Cities (Delete Edge from Graph)";
+	cout<<"\nPress 4: Delete City from DataBase (Delete Vertex from Graph)";
+	cout<<"\nPress 5: Out-Going Traffic (Out-Degree)";
+	cout<<"\nPress 6: In-Coming Traffic (In-Degree)";
+	cout<<"\nPress 7: Traversal";
+	cout<<"\n----------------------------------------------------------------";
+}
+int main(){
+		Graph flights;
+		string source,destination,city;
+		float cost;
+		int choice;
+		flights.menu();
+		cout<<"\nEnter Choice [99-INDEX|0-EXIT]: ";
+		cin>>choice;
+		while(choice){
+			switch(choice){
+			case 1:	cout<<"\nEnter City Name: ";cin>>city;
+					flights.addVertex(city);
+					break;
+			case 2: cout<<"\nEnter Source City: ";cin>>source;
+					cout<<"\nEnter Destination City: ";cin>>destination;
+					cout<<"\nEnter Cost: ";cin>>cost;
+					flights.addEdge(source,destination,cost);
+					break;
+
+			}
+		}
+}
+//int main() {
+//	Graph flights;
+//	flights.addVertex("Pune");
+//	flights.addVertex("Mumbai");
+//	flights.addVertex("Nashik");
+//	flights.addEdge("Pune","Mumbai",50);
+//	flights.addEdge("Pune","Nashik",60);
+//
+//	flights.addEdge("Mumbai","Pune",70);
+//	flights.addEdge("Mumbai","Nashik",80);
+//
+//	flights.addEdge("Nashik","Mumbai",85);
+//	flights.addEdge("Nashik","Pune",88);
+//
+//	flights.display();
+//	cout<<"\n"<<flights.inDegree("Pune");
+//
+//	return 0;
+//}
