8e13c424507fee0cf16693bb377ac1d298672c92
NearLawiet <sooraj1999vs@gmail.com>
2020-02-28 15:41:48 +0530

    On The Right Track

--- a07.cpp
+++ a07.cpp
@@ -1,43 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define MAX 29
+#define MAX 10
 
-/**************************************************************************************************/
-
-class Node{
+class Column{
 	string identifier;
 	int scope;
 	string type;
 	string value;
-	Node *link;
 public:
-	Node();
-	Node(string,int,string,string);
+	int chain;
+	Column();
+	Column(string,int,string,string);
 	void print();
-	friend class List;
 	friend class Table;
 };
-
-
-Node::Node(){
-	this->identifier[0]='\0';
-	this->scope=0;
-	this->type[0]='\0';
-	this->value[0]='\0';
-	link=NULL;
+Column::Column(){
+	identifier[0]='\0';
+	scope=0;
+	type[0]='\0';
+	value[0]='\0';
+	chain=-1;
 }
-
-
-Node::Node(string identifier, int scope, string type, string value){
+Column::Column(string identifier,int scope,string type,string value){
 	this->identifier=identifier;
 	this->scope=scope;
 	this->type=type;
 	this->value=value;
-	link=NULL;
+	chain=-1;
 }
-
-
-void Node::print(){
+void Column::print(){
 	cout<<"\n\t| Identifier |  "<<identifier;
 	if(scope==0)
 		cout<<"\n\t| Scope      |  Global";
@@ -47,179 +38,52 @@ void Node::print(){
 	cout<<"\n\t| Value      |  "<<value;
 }
 
-/**************************************************************************************************/
-
-class List{
-	Node *start;
-public:
-	List(){start=NULL;}
-	void pushFront(string,int,string,string);
-	void pushFront(Node*);
-	void pushBack(string,int,string,string);
-	void pushBack(Node*);
-	Node* popFront();
-	bool deleteNode(string,int);
-	void print();
-	bool searchNode(string);
-	bool declared(string,int);
-	friend class Table;
-};
 
 
-void List::pushFront(string identifier,int scope,string type,string value){
-	Node* temp=new Node(identifier,scope,type,value);
-	temp->link=start;
-	start=temp;
-	return;
-}
-
-
-void List::pushFront(Node *temp){
-	temp->link=start;
-	start=temp;
-	return;
-}
-
-
-void List::pushBack(string identifier,int scope,string type,string value){
-	Node *temp=new Node(identifier,scope,type,value);
-	if(start==NULL){
-		start=temp;
-		return;
-	}
-	Node *p=start;
-	for(p=start;p->link!=NULL;p=p->link);
-	p->link=temp;
-	return;
-}
-
-
-void List::pushBack(Node *temp){
-	if(start==NULL){
-		start=temp;
-		return;
-	}
-	Node *p=start;
-	for(p=start;p->link!=NULL;p=p->link);
-	p->link=temp;
-	return;
-}
-
-
-Node* List::popFront(){
-	Node *p=start;
-	start=start->link;
-	p->link=NULL;
-	return p;
-}
-
-
-void List::print(){
-	for(Node* p=start;p!=NULL;p=p->link)
-		p->print();
-}
-
-
-bool List::searchNode(string identifier){
-	bool flag=false;
-	for(Node *p=start;p!=NULL;p=p->link){
-		if(p->identifier==identifier){
-			p->print();
-			cout<<"\n";
-			flag=true;
-		}
-	}
-	return flag;
-}
-
-
-bool List::declared(string identifier,int scope){
-	for(Node *p=start;p!=NULL;p=p->link){
-		if(p->identifier==identifier&&p->scope==scope)
-			return true;
-	}
-	return false;	
-}
-
-
-bool List::deleteNode(string identifier,int scope){
-	if(start==NULL)
-		return false;
-	Node *p=NULL;
-	Node *q=start;
-	for(q=start;q!=NULL;q=q->link){
-		if(q->identifier==identifier&&q->scope==scope)
-			break;
-		p=q;
-	}
-	if(q==NULL)
-		return false;
-	else if(p==NULL){
-		p=start;
-		start=start->link;
-		p->link=NULL;
-		delete p;
-		return true;
-	}
-	else{
-		p->link=q->link;
-		q->link=NULL;
-		delete q;
-		return true;
-	}
-}
-
-/**************************************************************************************************/
 
 class Table{
-	List row[MAX];
+	Column row[MAX];
+	int n;
 public:
-	void insertWithReplacement(string,int,string,string);
-	void insertWithoutReplacement(string,int,string,string);
-	void searchIdentifier(string);
-	void deleteIdentifier(string,int);
-	bool modifyIdentifier(string,int,string,string);
-	int hashFunction(string identifier);
-};
+	Table(){n=0;}
+	int hashFunction(string);
+	void print();
+	bool isFull(){n==MAX-1;}
+	bool insertWithoutReplacement(Column);
+	bool insertWithReplacement(Column);
+}
+
+void Table::print(){
+	cout<<"\nScope\tType\tIdentifier\tVlaue\t\tChain";
+	for(int i=0;i<MAX;i++){
+		cout<<"\n"<<row[i].scope;
+		cout<<"\t"<<row[i].type;
+		cout<<"\t"<<row[i].identifier;
+		cout<<"\t"<<row[i].value;
+		cout<<"\t\t"<<row[i].chain;
+	}
+}
+
+
 int Table::hashFunction(string identifier){
 	int sum=0;
 	for(int i=0;i<identifier.length();i++)
-		sum=sum+identifier;
-		
+		sum=sum+identifier[i];
 	return sum%MAX;
 }
-void Table::insertWithoutReplacement(string identifier,int scope,string type,string value){
-	Node *data=new Node(identifier,scope,type,value);
-	int index=hashFunction(identifier);
-	row[index].pushBack(data);
+
+bool Table::insertWithoutReplacement(Column data){
+	if(this->isFull())
+		return false;
+	int id=hashFunction(data.identifier);
+	if(row[id].identifier.length()==0){
+		row[id]=data;
+		return true;
+	}
+	if(hash==hashFunction(row[id].identifier)){
+		int i=id;
+		while(row[i].chain!=-1){
+			i=row[i]
+		}
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
